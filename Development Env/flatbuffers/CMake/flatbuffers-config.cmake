26cdd5673e27d01ba5fa58332b0fdcdf054e479e
Joseph <jbartoszczyk@uiowa.edu>
2024-03-28 11:36:09 -0500

    init?

---
+++ Development Env/flatbuffers/CMake/flatbuffers-config.cmake
@@ -0,0 +1,4 @@
+include("${CMAKE_CURRENT_LIST_DIR}/FlatBuffersTargets.cmake" OPTIONAL)
+include("${CMAKE_CURRENT_LIST_DIR}/FlatcTargets.cmake" OPTIONAL)
+include("${CMAKE_CURRENT_LIST_DIR}/FlatBuffersSharedTargets.cmake" OPTIONAL)
+include("BuildFlatBuffers.cmake" OPTIONAL)
